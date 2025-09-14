0ab684bd2c85d160fd3f4d70ab5d69de548a3b4a
Saymakh88 <ksayma983@gmail.com>
2025-09-14 17:34:17 +0530

    Set the environment variables

--- src/lib/db.jsx
+++ src/lib/db.jsx
@@ -2,9 +2,9 @@ import mysql from "mysql2/promise";
 
 export async function connectDB() {
   return mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Root@1234",
-    database: "schoolDB",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 }
